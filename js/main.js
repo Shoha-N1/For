329877bbd73306e3ca5226d86f8aeaf35ef77b86
Shoha-N1 <abduvohidovs85@gmail.com>
2023-01-09 15:57:11 +0500

    For loop

---
+++ js/main.js
@@ -0,0 +1,142 @@
+
+// let a = 10;
+// let sum = 0;
+
+// for (let i = 0; i <= a; i++) {
+    
+//     sum = sum + i
+
+// }
+
+// console.log(sum)
+
+
+
+// let a = 10;
+// let sum = 0;
+
+// for (let i = 0; i <= a ; i++) {
+    
+//     if (i % 2 == 0){
+//         sum = sum +i
+//     }
+// }
+
+// console.log(sum)
+
+
+// let a = 10;
+// let sum = 0;
+
+// for (let i = 0; i <= a ; i++) {
+    
+//     if (i % 2 == 1){
+//         sum = sum +i
+//     }
+// }
+
+// console.log(sum)
+
+
+// let a = 35;
+// let sum = 0;
+
+// for (let i = 0; i <= a ; i++) {
+    
+//     if (i % 5 == 0){
+//         sum = sum +i
+//     }
+// }
+
+// console.log(sum)
+
+
+// let a = 10;
+// let count = 0;
+
+// for (let i = 0; i < a ; i++) {
+    
+//     if (i % 2 == 0){
+//         count = count + 1
+//     }
+// }
+
+// console.log(count)
+
+
+// let a = 2;
+// let b = 8;
+// let sum = 0;
+
+// for (let i = a; i < b ; i++) {
+    
+//     sum = sum + i
+// }
+
+// console.log(sum)
+
+
+// let a = 4;
+// let b = 15;
+// let sum = 0;
+
+// for (let i = a; i < b ; i++) {
+    
+//     if( i % 2 == 0 ){
+//         sum += 1
+//     }
+    
+// }
+
+// console.log(sum)
+
+
+// let a = 24;
+// let b = 31;
+// let count = 0;
+// let sum = 0;
+
+
+// for (let i = a; i < b ; i++) {
+    
+//     if( i % 3 == 0 ){
+//         count ++
+//         sum +=i
+//     }
+    
+// }
+
+// console.log("yig'indi = " +sum , "soni = " +count )
+
+
+
+// let a = 6;
+// let b = 16;
+// let sum = 0;
+
+// for (let i = a; i < b ; i++) {
+    
+//     if( i % 2 == 0 && i % 3 == 0 ){
+//         sum = sum + 1
+//     }
+    
+// }
+
+// console.log(sum)
+
+
+// let a = -1;
+// let b = 10;
+// let sum = 0;
+
+// for (let i = a; i <= b ; i++) {
+    
+//     if( i > 0){
+//         sum ++
+//         // yoki
+//         // sum +=1
+//     }
+    
+// }
+
+// console.log(sum)

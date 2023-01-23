5a595b902aa0552c94a7f08f8c528bce31d00ec1
Shoha-N1 <abduvohidovs85@gmail.com>
2023-01-23 13:08:00 +0500

    Commit

--- js/main.js
+++ js/main.js
@@ -140,3 +140,223 @@
 // }
 
 // console.log(sum)
+
+
+
+// let a = 2;
+// let b = 1;
+
+// for (let i = 0; i < 5; i++) {
+    
+//     b = b * a
+
+// }
+
+// console.log(b)
+
+
+
+
+// let a = 2
+// let n = 6
+// let b = 1
+
+// for (let i = 0; i < n; i++) {
+    
+//     b = b * a
+    
+// }
+
+// console.log(b)
+
+
+
+// let a = 3
+// let n = 4
+// let t = 0
+// let s = 0
+
+// for (let i = 0; i < n; i++) {
+    
+//     t = t * 10 + a
+//     s = s + t
+    
+// }               
+
+// console.log(s)
+
+
+
+//   14 - Mukkam son o'xshamadi
+
+// let a = 6
+// let sum = 0
+
+// for (let i = 1; i < a; i++) {
+   
+//     if (a % 1 == 0){
+//         sum = sum + i;
+
+        
+//     }
+//      if ( a == sum){
+//         console.log("true") 
+//     }
+    
+//     else {
+//        console.log("false")
+//     }
+
+
+// }
+
+
+
+
+//   Ketma-ketlik kvadradi 
+
+// let n = 6
+// let count = 0
+
+// for  (let i = 1; i <= n; i++) {
+    
+//     count = count + i * i
+    
+// }
+
+// console.log(count)
+
+
+
+   //  Armstrong rqami 
+
+   // function isArmstrongNumber(num) {
+   //    let numStr = num.toString();
+   //    let sum = 0
+   //    for (let i = 0; i < numStr.length; i++) {
+   //       sum +=Number(numStr[i]) ** numStr.length;
+   //    }
+   //    return sum === num
+   // }
+
+   // console.log(isArmstrongNumber(256))
+
+
+
+   // Tub son uxshamadi 
+
+//                 
+// let n = 7;      
+// let count = 0;
+
+// for (let i = 1; i <= n.length; i++) {
+//     if ( n % i == 0){
+//         count++ && count == 2
+
+//         'true'
+//     }else {
+//         'false'
+//     }
+    
+// }
+
+// console.log(count)
+
+
+
+// Son necha xonali  - o'xshamadi
+
+// let n = 100
+// let count = 0 
+
+
+// for (temp != 0; temp <= n ; temp++) {
+    
+//     if (n != 0){
+//         count ++
+//         n = n / 10
+//     }
+    
+// }
+
+// console.log(count)
+
+
+
+   // Raqamlar yig'indis  -- o'xshamadi
+
+// let n = 81
+// let count = 0;
+// let sum = 0 ;
+// let temp = n ;
+
+// for (let i = 0; i <= n ; i++) {
+    
+//     if ( temp != 0){
+//         sum = sum + temp % 10
+//         temp = temp / 10
+//     }
+// }
+
+// console.log (sum)
+
+
+
+
+//  Teskari son  -- o'xshamadi
+
+// let n = 12345 ;
+// let reversNum = 0;
+// let temp;
+
+// for (let i = 0; i <= n; i++) {
+    
+//    if(n != 0){
+//     temp = n % 10
+//     reversNum = reversNum * 10 + temp
+    
+//    }
+    
+// }
+
+// console.log(reversNum)
+
+
+
+
+     // Sonning - teskarisi   -- o'xshamadi
+
+   //   let n = 12321
+   //   let num = n;
+   //   let temp;
+   //   let reversNum = 0;
+
+   //   for (; num != 0;) {
+   //    temp = num % 10
+   //    reversNum = reversNum * 10 + temp
+      
+   //    num = num / 10
+
+   //    if(n == reversNum){
+   //       console.log("true")
+   //  }else {
+   //      console.log("false")
+   //  }
+   //   }
+
+
+
+   // Fatarial   
+
+   // let n = 5;
+   // let f = 1;
+
+   // for (let i = 1; i <= n; i++) {
+     
+   //    f = f * i
+   // }
+
+   // console.log(f)
+
+
+   
